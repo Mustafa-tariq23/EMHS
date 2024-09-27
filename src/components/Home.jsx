1afa862b28ad7912b65ab01e980edbab434dcde6
anees-tech <aneesliaqat555@gmail.com>
2024-09-28 02:00:38 +0500

    FAQs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header'
 import ServicesGrid from './homepageSections/ServicesGrid'
 import AboutSection from './homepageSections/AboutSection'
+import FAQs from './FAQs'
 
 const Home = () => {
   return (
     <>
       <Header />
       <ServicesGrid />
+      <FAQs />
       <AboutSection/>
     </>
   )
---
+++ src/components/TeamExperts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TeamExperts() {
+  return (
+    <>
+        <div className="our_Team">
+            <h3>Our Expert Team</h3>
+            <p></p>
+        </div>
+    </>
+  )
+}
+
+export default TeamExperts
