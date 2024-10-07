b93f8e79413a766d023d1a503f23bd8e22543439
Mustafa Tariq <mt2032303@gmail.com>
2024-10-07 22:25:33 +0500

    SubServiceAPI complete

--- src/components/SubServiceAPI.js
+++ src/components/SubServiceAPI.js
@@ -6,10 +6,10 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
   {
     id: 1,
@@ -18,10 +18,10 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
   {
     id: 1,
@@ -30,10 +30,10 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
   {
     id: 1,
@@ -42,10 +42,10 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
   {
     id: 1,
@@ -54,10 +54,10 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
   {
     id: 1,
@@ -66,10 +66,10 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
   {
     id: 1,
@@ -78,9 +78,9 @@ const SubServiceAPI = [
     img1: "", // Add image here
     title2: "Cognitive Behavioral Therapy",
     title2Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img2: "", // Add image here
     title3: "Dialectical Behavior Therapy",
     title3Detail: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been Lorem ",
-    img1: "", // Add image here
+    img3: "", // Add image here
   },
 ]
